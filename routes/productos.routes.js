33c61aa48002be22a08da5930af695ccd70dcf42
Dany <danycambrano1@gmail.com>
2023-07-04 07:25:39 -0600

    Función para guardar, eliminar y las rutas

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -6,5 +6,8 @@ const router=routerx();
 //rutas para el controlador producto
 router.post('/guardarProducto',productosController.addProduct);
 router.get("/consultarProducto",productosController.consultarProducto);
+router.delete("/eliminarProducto/:id",productosController.eliminarProducto);
+router.patch("/updateProducto/:id",productosController.updateProducto);
+router.get("/oneProducto/:id",productosController.consultarOneProduct);
 
 export default router;
